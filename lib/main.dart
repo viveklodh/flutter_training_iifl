65cd0902a1792366124783bf9d2879ce82f23f18
vivek lodh <viveklodh55@gmail.com>
2023-05-29 14:40:29 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_training_iifl/pages/columnExample.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ColumnExample(),
+    );
+  }
+}
